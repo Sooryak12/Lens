2bd9621c986c7d34bcba6cccc195f5edbb0eb2b6
Soorya Prakash <sooryaprakash84@gmail.com>
2021-09-09 23:39:56 +0530

    Update config.py

--- recognizer/tools/config/config.py
+++ recognizer/tools/config/config.py
@@ -1,16 +1,7 @@
-# -*- coding=utf-8 -*-
-
-
 def get_lines():
     return len(open('recognizer/tools/dictionary/chars.txt', 'r',encoding='utf-8').readlines())
 
 
 num_class = get_lines() + 1
-
-GPU_MEMORY_FRACTION = 0.8
-
-TF_ALLOW_GROWTH = True
-
-is_debug = True
 
 
